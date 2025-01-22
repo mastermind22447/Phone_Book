1d74591679a68264507bc99220f159a76c0683e0
MasterMind <sjd.hadadian@gmail.com>
2025-01-22 16:35:21 +0330

    ExceptionHandler Added.

--- src/main/java/com/demo/phonebook/GlobalExceptionHandler.java
+++ src/main/java/com/demo/phonebook/GlobalExceptionHandler.java
@@ -2,13 +2,8 @@ package com.demo.phonebook;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * This class handles global exceptions for REST controllers.
